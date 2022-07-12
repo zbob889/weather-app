e4fec50a4f1e7393a26c7ff34a6bcbc76edb5b22
zbob889 <zroberts889@gmail.com>
2022-07-12 17:12:20 +0200

    HTML is done.  Now I need to figure out how to get the form value to save

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+const searchBar = document.getElementById('searchBar');
+
 async function getWeather(){
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Arnhem&APPID=6c4b9b17c0b21ea22dbbcf808f49caf2`, {mode: 'cors'});
     const weatherData = await response.json();
     console.log(weatherData);
 };
+
+searchBar.addEventListener('submit', () => {
+    localStorage.setItem('location', searchBar.value);
+});
