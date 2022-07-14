aec8294a7ca852c031a00972b6a027dbeee60f02
zbob889 <zroberts889@gmail.com>
2022-07-14 14:09:35 +0200

    Finally figured out the form submission

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-const searchBar = document.getElementById('searchBar');
-
 async function getWeather(){
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Arnhem&APPID=6c4b9b17c0b21ea22dbbcf808f49caf2`, {mode: 'cors'});
     const weatherData = await response.json();
     console.log(weatherData);
 };
 
-searchBar.addEventListener('submit', () => {
-    localStorage.setItem('location', searchBar.value);
+const searchBar = document.getElementById('searchBar');
+const submit = document.getElementById('submit');
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let location = document.getElementById('searchBar').value;
+    console.log(location);
+    localStorage.setItem('location', location);
 });
