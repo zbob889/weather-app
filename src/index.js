19b8fb25768d63574c3be04bedf4b820264930d9
zbob889 <zroberts889@gmail.com>
2022-07-14 14:42:21 +0200

    made it into a webpack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-async function getWeather(){
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Arnhem&APPID=6c4b9b17c0b21ea22dbbcf808f49caf2`, {mode: 'cors'});
+
+
+
+async function getWeather(city){
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=6c4b9b17c0b21ea22dbbcf808f49caf2`, {mode: 'cors'});
     const weatherData = await response.json();
     console.log(weatherData);
 };
@@ -11,6 +14,9 @@ const form = document.getElementById('form');
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     let location = document.getElementById('searchBar').value;
-    console.log(location);
     localStorage.setItem('location', location);
+    searchBar.value = '';
+
+    getWeather(location);
 });
+
